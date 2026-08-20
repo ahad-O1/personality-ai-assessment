76819106e50a2d5a54ff0b855549302384089e29
Muhammad Ahad Sharafat <ahadsharafat36@gmail.com>
2026-08-20 23:22:33 +0500

    Update WhiteNoise storage and startup.sh for Azure App Service

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-gunicorn --bind=0.0.0.0:8000 --timeout 600 personality_ai.wsgi
+gunicorn --bind=0.0.0.0:${PORT:-8000} --timeout 600 personality_ai.wsgi:application
