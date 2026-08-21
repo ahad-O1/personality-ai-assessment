1708e6ff804b47d7b314ff02f86f80ce22966a14
Muhammad Ahad Sharafat <ahadsharafat36@gmail.com>
2026-08-21 22:49:58 +0500

    Auto import questions and careers on Azure startup

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+python import_questions.py
+python import_careers.py
 gunicorn --bind=0.0.0.0:${PORT:-8000} --timeout 600 personality_ai.wsgi:application
